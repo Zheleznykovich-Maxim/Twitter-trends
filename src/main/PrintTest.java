0e23a127658fbca62e0cb8b2014c9167966d2726
Zheleznykovich-Maxim <maxssmail572@gmail.com>
2023-03-22 16:12:32 +0300

    Рисование карты сша

--- src/main/PrintTest.java
+++ src/main/PrintTest.java
@@ -18,27 +18,23 @@ public class PrintTest extends JPanel {
 
             ArrayList<ArrayList<ArrayList<Double>>> state = coords.get(key);
             if (state.get(0).get(0).size() != 2) {
+                System.out.println("ЧЕТЫРЁХМЕРНЫЙ МАССИВ");
                 ArrayList<ArrayList<ArrayList<ArrayList<Double>>>> state4 = coords.get(key);
-                for (int i = 0; i < state4.get(0).size(); i++) {
-                    int[] fillPolygon = new int[state4.get(0).get(i).size()];
-                    int[] ints = new int[state4.get(0).get(0).size()];
-                    for (int j = 0; j < state4.get(0).get(i).size(); j++){
-                        fillPolygon[j] = (int) ((-state4.get(0).get(i).get(0).get(1) + 74.9) * 10.8);
-                        ints[j] = (int) ((state4.get(0).get(i).get(0).get(0) + 180.0) * 10.8);
+                for (int i = 0; i < state4.size(); i++) {
+                    int[] fillPolygon = new int[state4.get(i).get(0).size()];
+                    int[] ints = new int[state4.get(i).get(0).size()];
+                    for (int j = 0; j < state4.get(i).get(0).size(); j++){
+                        fillPolygon[j] = (int) ((-state4.get(i).get(0).get(j).get(1) + 74.9) * 10.8);
+                        ints[j] = (int) ((state4.get(i).get(0).get(j).get(0) + 180.0) * 10.8);
+
                     }
-                    g.drawPolygon(ints, fillPolygon, ints.length);
+                    Polygon test = new Polygon(ints, fillPolygon, ints.length);
+                    Graphics2D g2d = (Graphics2D) g;
+                    g2d.setPaint(Color.black);
+                    g2d.fill(test);
+                    g.drawPolygon(test);
                 }
-//                int[] fillPolygon = new int[state4.get(0).get(0).size()];
-//                int[] ints = new int[state4.get(0).get(0).size()];
-//                System.out.println(state4.toString());
-//                for (int i = 0; i < state4.get(0).get(0).size(); i++) {
-//                    System.out.println(state4.get(0).get(0).get(i));
-//                    System.out.println(state4.get(0).get(0).size());
-//                    System.out.println("ШТАТ = " + key);
-//                    fillPolygon[i] = (int) ((-state4.get(0).get(0).get(i).get(1).doubleValue() + 74.9) * 10.8);
-//                    ints[i] = (int) ((state4.get(0).get(0).get(i).get(0) + 180.0) * 10.8);
-//                }
-//                g.drawPolygon(ints, fillPolygon, ints.length);
+
             }
             else {
                 int[] fillPolygon = new int[state.get(0).size()];
